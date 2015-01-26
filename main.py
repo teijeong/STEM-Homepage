6dcf18dd5b7e678218060a941b1431b3735c67cd
Taehee Jeong <wwee3631@gmail.com>
2015-01-26 03:07:29 -0500

    [20150126] Working on asp to Jinja2

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://be3278fbbccac7:64de4292@us-cdbr-iron-east-01.cleardb.net/heroku_60fb971363678a5'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+    
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
