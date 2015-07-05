3c1f19f5fdcf5c27efdd8efa72eed3e579fc3a30
Taehee Jeong <wwee3631@gmail.com>
2015-07-06 02:44:35 +0900

    [20150706] Bug fix

--- app/static/adminLTE/dist/js/member_app.js
+++ app/static/adminLTE/dist/js/member_app.js
@@ -6,68 +6,75 @@ stemApp.config(['$interpolateProvider', function($interpolateProvider) {
 }]);
 
 var person = function(id, name, img) {
-	this.id = id;
-	this.name = name;
-	this.picture = 'images/' + img;
-	this.department = 'Department';
-	this.description = 'Jon loves to longboard, ride fixie, and drive stick because, as he says, the journey is just as important as the destination.';
-	this.social = 'https://facebook.com/FredJeong';
-	this.cover = '';
+    this.id = id;
+    this.name = name;
+    this.picture = 'images/' + img;
+    this.department = 'Department';
+    this.description = 'Jon loves to longboard, ride fixie, and drive stick because, as he says, the journey is just as important as the destination.';
+    this.social = 'https://facebook.com/FredJeong';
+    this.cover = '';
 }
 
 var memberListScope;
 
 stemApp.controller('memberList', function($scope, $timeout) {
-	memberListScope = $scope;
-	$scope.members = [];
-	$scope.pIndex = 0;
-	$scope.openCard = function(id) {openCard($scope, id);};
-	jQuery.ajax({
-		url: "/people",
-		type: "GET",
-		success: function(data){
-			$scope.members = data;
-			$scope.$apply();
-		}
-	});
-	$timeout(function(){
-		(function($) {
-			$(".member-picture-thumb").each(function(i, elem) {
-				console.log(elem);
-				if (elem.naturalWidth < elem.naturalHeight)
-					$(this).addClass("portrait");
-			});
-		})(jQuery);}, 500);
-	$scope.prevMember = function(){return prevMember($scope);};
-	$scope.nextMember = function(){return nextMember($scope);};
-	$scope.goNext = function(){goNext($scope);};
-	$scope.goPrev = function(){goPrev($scope);};
+    memberListScope = $scope;
+    $scope.members = [];
+    $scope.pIndex = 0;
+    $scope.openCard = function(id) {openCard($scope, id);};
+    jQuery.ajax({
+        url: "/people",
+        type: "GET",
+        success: function(data){
+            $scope.members = data;
+            $scope.all_members = [[data[0]]];
+            var cnt = 0;
+            for (var i=1; i < data.length; i++) {
+                if (data[i-1].cycle !== data[i].cycle) {
+                    cnt++;
+                    $scope.all_members.push([data[i]]);
+                } else $scope.all_members[cnt].push(data[i]);
+            }
+            $scope.$apply();
+        }
+    });
+    $timeout(function(){
+        (function($) {
+            $(".member-picture-thumb").each(function(i, elem) {
+                if (elem.naturalWidth < elem.naturalHeight)
+                    $(this).addClass("portrait");
+            });
+        })(jQuery);}, 500);
+    $scope.prevMember = function(){return prevMember($scope);};
+    $scope.nextMember = function(){return nextMember($scope);};
+    $scope.goNext = function(){goNext($scope);};
+    $scope.goPrev = function(){goPrev($scope);};
 });
 
 function openCard($scope, id) {
-	for (var i in $scope.members) {
-		if($scope.members[i].id === id) {
-			$scope.pIndex = i;
-			jQuery(".member-card").trigger('openModal');
-			return;
-		}
-	}
+    for (var i = 0; i < $scope.members.length; i++) {
+        if($scope.members[i].id === id) {
+            $scope.pIndex = i;
+            jQuery(".member-card").trigger('openModal');
+            return;
+        }
+    }
 }
 
 function goNext($scope) {
-	$scope.pIndex++;
-	$scope.pIndex %= $scope.members.length;
+    $scope.pIndex++;
+    $scope.pIndex %= $scope.members.length;
 }
 
 function goPrev($scope) {
-	$scope.pIndex += $scope.members.length - 1;
-	$scope.pIndex %= $scope.members.length;
+    $scope.pIndex += $scope.members.length - 1;
+    $scope.pIndex %= $scope.members.length;
 }
 
 function nextMember($scope) {
-	return $scope.members[($scope.pIndex + 1) % $scope.members.length];
+    return $scope.members[($scope.pIndex + 1) % $scope.members.length];
 }
 
 function prevMember($scope) {
-	return $scope.members[($scope.pIndex + $scope.members.length - 1) % $scope.members.length];
+    return $scope.members[($scope.pIndex + $scope.members.length - 1) % $scope.members.length];
 }
