20f9c200e94390a0017c431b02e9d509876104ef
Taehee Jeong <wwee3631@gmail.com>
2015-08-19 17:19:30 +0900

    [20150819] Notification Generation

--- app/notification.py
+++ app/notification.py
@@ -1,4 +1,19 @@
 from app import models, db
+from app.models import ObjectType
+from app.models import NotificationAction as Verb
+import re
 
-def Push(sender, receivers, Target, Verb, Message):
-    pass
+def Push(sender, receivers, target, verb, message):
+    type_parser = re.compile('models\.(\w*)')
+    try:
+        target_type = type_parser.findall(str(type(target)))[0]
+    except IndexError:
+        print("Invalid Object type")
+    target_type = ObjectType[target_type]
+
+    for receiver in receivers:
+        noti = models.Notification(sender=sender,receiver=receiver,
+            object_type=target_type,object_id=target.id,
+            verb=verb,message=message)
+        db.session.add(noti)
+    db.session.commit()
